666a2fef281a21bb872718171602eb539d7d0e3b
Rakesh Das <rakeshdaskowshik@gmail.com>
2020-03-21 15:31:58 -0500

    Initial commit

---
+++ app.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+func (a *App) Initialize(user, password, dbname string) {
+	connectionString := fmt.Sprintf("host=192.168.1.150 port=5432 user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Router = mux.NewRouter()
+	a.initializeRoutes()
+}
+
+func (a *App) Run(addr string) {
+	log.Fatal(http.ListenAndServe(":8010", a.Router))
+}
+
+func (a *App) getTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid task ID")
+		return
+	}
+	t := task{ID: id}
+	if err := t.getTask(a.DB); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, "Task not found")
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+	}
+	respondWithJson(w, http.StatusOK, t)
+}
+func (a *App) getAllTasks(w http.ResponseWriter, r *http.Request) {
+	t := task{}
+	tasks, err := t.getAllTasks(a.DB)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+	}
+	respondWithJson(w, http.StatusOK, tasks)
+}
+func (a *App) initializeRoutes() {
+	a.Router.HandleFunc("/tasks", a.getAllTasks).Methods("GET")
+	a.Router.HandleFunc("/task/{id:[0-9]+}", a.getTask).Methods("GET")
+}
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJson(w, code, map[string]string{"error": message})
+}
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
---
+++ models.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type task struct {
+	completed             bool   `json:"completed"`
+	ID                    int    `json:"task_id"`
+	source                string `json:"source"`
+	destination           string `json:"destination"`
+	elapsedTime           string `json:"elapsed_time"`
+	startedAt             string `json:"started_at"`
+	endedAt               string `json:"ended_at"`
+	eta                   string `json:"eta"`
+	numberOfChecksDone    string `json:"number_of_checks_done"`
+	totalNumberOfChecks   string `json:"total_number_of_checks"`
+	numberOfFilesUploaded string `json:"number_of_files_uploaded"`
+	totalNumberOfFiles    string `json:"total_number_of_files"`
+	uploadedSize          string `json:"uploaded_size"`
+	totalSize             string `json:"total_size"`
+	transferSpeed         string `json:"transfer_speed"`
+	percentage            string `json:"percentage"`
+}
+
+func (t *task) getTask(db *sql.DB) error {
+	return db.QueryRow("SELECT * FROM backups.tasks WHERE task_id=$1", 36).Scan(&t.completed, &t.ID, &t.source, &t.destination, &t.elapsedTime, &t.startedAt, &t.endedAt, &t.eta, &t.numberOfChecksDone, &t.totalNumberOfChecks, &t.numberOfFilesUploaded, &t.totalNumberOfFiles, &t.uploadedSize, &t.totalSize, &t.transferSpeed, &t.percentage)
+}
+func (t *task) getAllTasks(db *sql.DB) ([]task, error) {
+	rows, err := db.Query("SELECT * FROM backups.tasks")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	tasks := []task{}
+	for rows.Next() {
+		var t task
+		if err := rows.Scan(&t.completed, &t.ID, &t.source, &t.destination, &t.elapsedTime, &t.startedAt, &t.endedAt, &t.eta, &t.numberOfChecksDone, &t.totalNumberOfChecks,
+			&t.numberOfFilesUploaded, &t.totalNumberOfFiles, &t.uploadedSize, &t.totalSize, &t.transferSpeed, &t.transferSpeed); err != nil {
+			return nil, err
+		}
+		fmt.Println(t)
+		tasks = append(tasks, t)
+	}
+	return tasks, nil
+}
