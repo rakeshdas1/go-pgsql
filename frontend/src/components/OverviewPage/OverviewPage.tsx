3c1a984e74272ee3808e5f1fee1945b2b14f0623
Rakesh Das <rakeshdaskowshik@gmail.com>
2020-06-22 14:45:54 -0500

    Renamed overview component

--- frontend/src/components/OverviewPage/OverviewPage.tsx
+++ frontend/src/components/OverviewPage/OverviewPage.tsx
@@ -1,11 +1,11 @@
-import { useState,useEffect } from "react";
-import { TaskModel } from "../../models/Task.model";
 import React from "react";
+import { useState, useEffect } from "react";
+import { TaskModel } from "../../models/Task.model";
 import { getLastRunTask, getNTasks } from "../../api/rcloneApi";
 import TaskComponent from "../Task/Task";
 import RecentTasksComponent from "../RecentTasks/RecentTasks";
 
-export const TaskPageComponent = () => {
+export const TasksOverviewComponent = () => {
     const [latestTask, setLatestTask] = useState<TaskModel>();
     const [recentTasks, setRecentTasks] = useState<TaskModel[]>();
     useEffect(() => {
@@ -19,15 +19,15 @@ export const TaskPageComponent = () => {
             .then(data => {
                 setRecentTasks(data);
             })
-    },[])
+    }, [])
     return (
-        <div className="task-page">
-            <h1>Last run task: </h1>
-            <TaskComponent task={latestTask}></TaskComponent>
-            <hr></hr>
-            <h1>Recent tasks run:</h1>
-            <RecentTasksComponent recentTasks ={recentTasks}></RecentTasksComponent>
-        </div>
+            <div className="task-page">
+                <h1>Last run task: </h1>
+                <TaskComponent task={latestTask}></TaskComponent>
+                <hr></hr>
+                <h1>Recent tasks run:</h1>
+                <RecentTasksComponent recentTasks={recentTasks}></RecentTasksComponent>
+            </div>
     )
 }
-export default TaskPageComponent;
+export default TasksOverviewComponent;
