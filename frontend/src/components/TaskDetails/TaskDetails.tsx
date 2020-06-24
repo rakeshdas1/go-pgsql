bcf9bd64ae0cfc139d71903336c77d98e65edfce
Rakesh Das <rakeshdaskowshik@gmail.com>
2020-06-24 10:59:29 -0500

    Added message when there are no files in a task

--- frontend/src/components/TaskDetails/TaskDetails.tsx
+++ frontend/src/components/TaskDetails/TaskDetails.tsx
@@ -6,10 +6,10 @@ import FileDetailsComponent from './FilesTable';
 import './TaskDetails.css';
 import { FileModel } from '../../models/File.model';
 import { RouteComponentProps } from 'react-router-dom';
-interface TaskDetailsComponentRouteParams extends RouteComponentProps<{ taskId: string }>{
+interface TaskDetailsComponentRouteParams extends RouteComponentProps<{ taskId: string }> {
 }
 const socket = new WebSocket('ws://localhost:8010/currentRunningTask');
-export const TaskDetailsComponent = (props:TaskDetailsComponentRouteParams) => {
+export const TaskDetailsComponent = (props: TaskDetailsComponentRouteParams) => {
     const [task, setTask] = useState<TaskModel>();
     const [taskFiles, setTaskFiles] = useState<FileModel[]>([]);
     useEffect(() => {
@@ -18,10 +18,10 @@ export const TaskDetailsComponent = (props:TaskDetailsComponentRouteParams) => {
                 setTask(data);
                 getFilesForTask(100, 0, data.taskId)
                     .then(data => setTaskFiles(data));
-                if(data.completed === false) {
+                if (data.completed === false) {
                     socket.onmessage = (evt: MessageEvent) => {
-                        const data:File = JSON.parse(evt.data);
-                        console.log(data); 
+                        const data: File = JSON.parse(evt.data);
+                        console.log(data);
                     }
                 }
             });
@@ -29,45 +29,51 @@ export const TaskDetailsComponent = (props:TaskDetailsComponentRouteParams) => {
             socket.close();
         }
     }, [props.match.params.taskId]);
-    const epochToLocalTime = (epochTime?: string):Date => {
+    const epochToLocalTime = (epochTime?: string): Date => {
         let d: Date = new Date(0);
-        let epochSeconds:number = Number(epochTime);
+        let epochSeconds: number = Number(epochTime);
         d.setUTCSeconds(epochSeconds);
         return d;
     }
-    const startAndEndTimeItems = (startTime?:string, endTime?:string) => {
-        const startTimeDate:Date = epochToLocalTime(startTime);
-        const endTimeDate:Date = epochToLocalTime(endTime);
+    const startAndEndTimeItems = (startTime?: string, endTime?: string) => {
+        const startTimeDate: Date = epochToLocalTime(startTime);
+        const endTimeDate: Date = epochToLocalTime(endTime);
         return (
             <div>
                 <Header size='medium' className="dateTimes">
-                <Icon name='clock'/>
-                    Started task: 
+                    <Icon name='clock' />
+                    Started task:
                     <Popup content={startTimeDate.toString()}
-                    trigger={<p> {startTimeDate.toLocaleTimeString()}</p>}/>     
+                        trigger={<p> {startTimeDate.toLocaleTimeString()}</p>} />
                 </Header>
                 <Header size='medium' className="dateTimes">
-                <Icon name='clock'/>
-                    Ended task: 
+                    <Icon name='clock' />
+                    Ended task:
                     <Popup content={endTimeDate.toString()}
-                    trigger={<p> {endTimeDate.toLocaleTimeString()}</p>}/>     
+                        trigger={<p> {endTimeDate.toLocaleTimeString()}</p>} />
                 </Header>
             </div>
         );
     }
-    return(
+    return (
         <div>
             <Header size='huge' textAlign='center'>Task # {task?.taskId}</Header>
             <Grid divided='vertically'>
                 <Grid.Row columns={2}>
                     <div>
-                        <Header size='medium'><Icon name='folder'/>Source: {task?.source}</Header>
-                        <Header size='medium'><Icon name='cloud upload'/>Destination: {task?.destination}</Header>
-                        <Header size='medium'><Icon name='file'/>Number of files: {task?.totalNumberOfChecks}</Header>
-                        <Header size='medium'><Icon name='database'/>Total Size: {task?.totalSize}</Header>
+                        <Header size='medium'><Icon name='folder' />Source: {task?.source}</Header>
+                        <Header size='medium'><Icon name='cloud upload' />Destination: {task?.destination}</Header>
+                        <Header size='medium'><Icon name='file' />Number of files: {task?.totalNumberOfChecks}</Header>
+                        <Header size='medium'><Icon name='database' />Total Size: {task?.totalSize}</Header>
                         {startAndEndTimeItems(task?.startedAt, task?.endedAt)}
                         <Header size='medium'>File details:</Header>
-                        <FileDetailsComponent files={taskFiles}></FileDetailsComponent>
+                        {(!taskFiles.length) ? (
+                            <Header as="h1" disabled><Icon name='dont' />No files found for this task</Header>
+                        ) :
+                            (
+                                <FileDetailsComponent files={taskFiles}></FileDetailsComponent>
+                            )
+                        }
                     </div>
                 </Grid.Row>
             </Grid>
